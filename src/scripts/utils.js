dce0d41628c21210658030e4d618a6c96cab644a
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-11-06 15:38:36 +1100

    Add utils.js

---
+++ src/scripts/utils.js
@@ -0,0 +1,22 @@
+/**
+ * @file utils.js
+ * @description A compilation of utility functions used across different Chrome scripts
+ */
+
+export {
+    asyncWrapper,
+};
+
+/**
+ * @description Provides a mechanism for abstracting asynchronous error handling
+ * @param {Promise} promise An asynchronous operation
+ * @returns {[Promise, Error]} [response, null] if the promise resolves, [null, error] if the promise rejects
+ */
+const asyncWrapper = async (promise) => {
+    try {
+        const response = await promise;
+        return [response, null];
+    } catch(error) {
+        return [null, error]
+    }
+};
