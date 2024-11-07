5e9666ea746ae4d818333dcae0ee28d138507080
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-11-07 14:47:07 +1100

    Add settingsKeys enum and templates for updateSettings and getSettings

--- src/background/storage.js
+++ src/background/storage.js
@@ -20,16 +20,52 @@ const defaultSettingsTemplate = Object.freeze({
 });
 
 /**
+ * @description An enum of settings keys used for updateSettings and getSettings functions
+ */
+const settingsKeys = Object.freeze({
+    TIMER: "timer",
+    TIMER_HH: "hours",
+    TIMER_MM: "minutes",
+    TIMER_SS: "seconds",
+    BLOCK: "blocking",
+});
+
+/**
  * @description Initialises extension settings upon download or via import
  * @param {Object} data The default settings of the extension
  */
-const initSettings = async (data = { ...defaultSettingsTemplate }) => {
+const initSettings = async (data = defaultSettingsTemplate) => {
+
+    // STEP 1: If importing non-template settings, ensure all subkeys are valid
+
+    // STEP 2: Initialise extension settings
     const [,error] = await asyncWrapper(chrome.storage.local.set(data));
     if (error) {
         console.error("Failed to save settings in chrome.storage.local", error);
     }
 };
 
+/**
+ * @description Updates extension settings for either the timer or website blocking list
+ * @param {string} key "timer" or "blocking"
+ * @param {Object|Array<string>} keyData If `key` is "timer", keyData is Object, otherwise Array for "blocking"
+ */
+const updateSettings = async (key, keyData) => {
+
+};
+
+/**
+ * @description 
+ * @param {string} key (optional) either "timer" or "blocking"
+ * @returns {Object|Array<strings>} If there is no `key`, returns entire settings as an Object, if key is "timer" returns an Object, otherwise Array for "blocking" 
+ */
+const getSettings = async(key = null) => {
+
+};
+
 export { 
+    settingsKeys,
     initSettings,
+    updateSettings,
+    getSettings,
 };
