9be55fcb9f2ddd65e412d82e4f08df35ec7a46ce
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-11-09 16:10:13 +1100

    Add msgOps enum for options.js

--- src/options/operations.js
+++ src/options/operations.js
@@ -3,3 +3,25 @@
  * @description Implements logic for options.js operatons.
  */
 
+import asyncWrapper from "../scripts/utils";
+
+/**
+ * @description An enumeration for options messaging operations
+ */
+const msgOps = Object.freeze({
+    TIMER_S: "set-timer-settings",
+    TIMER_G: "get-timer-settings",
+    TIMER_R: "reset-timer-settings",
+
+    BLOCK_S: "set-blocking-settings",
+    BLOCK_G: "get-blocking-settings",
+    BLOCK_R: "reset-blocking-settings",
+
+    EXPORT: "export-settings",
+    IMPORT: "import-settings",
+    TOTAL_R: "total-reset",
+});
+
+export {
+    msgOps,
+}
