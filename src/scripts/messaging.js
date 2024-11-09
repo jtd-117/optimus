c5c106d721118b00dcaa0e7a8f68685a88f682f0
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-11-09 15:10:04 +1100

    Add sendMessage function

--- src/scripts/messaging.js
+++ src/scripts/messaging.js
@@ -7,18 +7,35 @@
 import asyncWrapper from "./utils";
 
 /**
- * @description Makes a message object
- * @param {string} opName The name of the operation to be sent to the target file
+ * @description Makes a message object used by the sender
+ * @param {string} opName The name of the operation to be sent to the target component
  * @param {Object} opData (optional) Any necessary data needed to perform `opName`
  * @returns {Object} The message to be sent via the chrome.runtime API
  */
-const makeMessage = (opName, opData = {}) => {
+const makeSenderMessage = (opName, opData = {}) => {
     return {
         operation: opName,
         ...opData,
     };
 }
 
+/**
+ * @description Sends a message to another Chrome extension component
+ * @param {string} opName The name of the operation to be sent to the target component
+ * @param {Object} opData (optional) Any necessary data needed to perform `opName`
+ * @param {function} callBack (optional) A callback to be used by the target component
+ * @returns {[Promise, Error]} A response, if any, sent by the target component
+ * @link {https://developer.chrome.com/docs/extensions/reference/api/runtime#method-sendMessage}
+ */
+const sendMessage = async (opName, opData, callBack = null) => {
+    const message = makeSenderMessage(opName, opData);
+    if (!callBack) {
+        return await asyncWrapper(chrome.runtime.sendMessage(message));
+    }
+    return await asyncWrapper(chrome.runtime.sendMessage(message, callBack));
+};
+
 export {
-    makeMessage,
+    makeSenderMessage,
+    sendMessage,
 };
