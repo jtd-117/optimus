2e9eb0c2147cf7f2f6fd7f4f7723156e5c336659
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-11-10 15:28:40 +1100

    Add setTimerSettings

--- src/options/operations.js
+++ src/options/operations.js
@@ -3,7 +3,9 @@
  * @description Implements logic for options.js operatons.
  */
 
+import ids from "./selectors";
 import asyncWrapper from "../scripts/utils";
+import * as msg from "../scripts/messaging";
 
 /**
  * @description An enumeration for options messaging operations
@@ -22,6 +24,28 @@ const msgOps = Object.freeze({
     TOTAL_R: "total-reset",
 });
 
+/**
+ * @description Sends a request to background.js to update timer settings
+ * @returns {Boolean} `true` if background.js successfully changed timer settings, `false` otherwise
+ */
+const setTimerSettings = async () => {
+    const hh = document.getElementById(ids.TIMER_HH).value;
+    const mm = document.getElementById(ids.TIMER_MM).value;
+    const ss = document.getElementById(ids.TIMER_SS).value;
+    const data = {
+        hours: hh,
+        minutes: mm,
+        seconds: ss,
+    };
+    const [, error] = await asyncWrapper(msg.sendMessage(msgOps.TIMER_S, data));
+    if (error) {
+        console.error(`Failed to ${msgOps.TIMER_S} in background.js`);
+        return false;
+    }
+    return true;
+};
+
 export {
     msgOps,
-}
+    setTimerSettings,
+};
