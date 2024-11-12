5eac3a03348ba9e6b32419d5443f9dcb471813c5
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-11-12 14:09:55 +1100

    Add export keyword  keyFiles

--- src/scripts/key-files.js
+++ src/scripts/key-files.js
@@ -2,10 +2,11 @@
  * @file key-files.js
  * @description An enumeration of the entry point JavaScript files in webpack.common.js
  */
-
 const keyFiles = Object.freeze({
     CONTENT: "content.js",
     BACKGROUND: "background.js",
     OPTIONS: "options.js",
     POPUP: "popup.js",
 });
+
+export default keyFiles;
