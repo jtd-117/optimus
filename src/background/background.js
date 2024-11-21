9977049494c401283bb69942d687dc391306f8fd
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-11-21 12:10:13 +1100

    Add template conditionals in background.js

--- src/background/background.js
+++ src/background/background.js
@@ -12,16 +12,17 @@ chrome.runtime.onInstalled.addListener(async () => {
     await stg.initSettings();
 });
 
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
     // Only ACCEPT messages from Optimus extension components
     if (sender.id === chrome.runtime.id && sender.url) {
         
         // CASE 1: Timer ACTIVATE
+        if (message.operation === operations.TIMER_A) {
+            console.log(`Operation received: ${operations.TIMER_A}`);
 
         // CASE 2: Timer GET
-        if (message.operation === operations.TIMER_G) {
+        } else if (message.operation === operations.TIMER_G) {
             rps.getTimerResponse(sendResponse);
 
         // CASE 3: Timer SET
@@ -29,15 +30,25 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             rps.setTimerResponse(sendResponse, message.data);
 
         // CASE 4: Block GET   
-        }
+        } else if (message.operation === operations.BLOCK_G) {
+            console.log(`Operation received: ${operations.BLOCK_G}`);
+
         // CASE 5: Block SET
+        } else if (message.operation === operations.BLOCK_S) {
+            console.log(`Operation received: ${operations.BLOCK_S}`);
 
         // CASE 6: Export Settings
+        } else if (message.operation === operations.EXPORT) {
+            console.log(`Operation received: ${operations.EXPORT}`);
 
         // CASE 7: Import Settings
+        } else if (message.operation === operations.IMPORT) {
+            console.log(`Operation received: ${operations.IMPORT}`);
 
         // CASE 8: Total Settings Reset
-
+        } else if (message.operation === operations.RESET) {
+            console.log(`Operation received: ${operations.RESET}`);
+        }
     }
     // Indicate that chrome.runtime.onMessage is asynchronous
     return true;
