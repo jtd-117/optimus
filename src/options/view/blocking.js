f46f3ccd584fe4b0f93c7d2b2d4fd0c55bfbec92
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2024-11-24 20:57:51 +1100

    Add clearBlockList and updateDisplay function for blocking.js

--- src/options/view/blocking.js
+++ src/options/view/blocking.js
@@ -4,3 +4,38 @@
  */
 
 import * as ems from "../utils/elements";
+
+/**
+ * @description HELPER - Removes all bullet point list tags in the block list element
+ */
+const clearBlockList = () => {
+    const blockList = ems.getBlockingElements().list;
+    blockList.textContent = "";
+};
+
+/**
+ * @description Updates the block list display via block settings retrieved from background.js
+ * @param {Array<string>} websites An array of strings representing user-selected blocked websites
+ */
+const updateDisplay = (websites) => {
+    const blockList = ems.getBlockingElements().list;
+    clearBlockList();
+
+    if (websites.length === 0) {
+        const emptyList = document.createElement("li");
+        emptyList.textContent = "No blocked websites";
+        blockList.appendChild(emptyList);
+
+    } else {
+        websites.forEach((website) => {
+            const bulletPoint = document.createElement("li");
+            bulletPoint.textContent = website;
+            blockList.appendChild(bulletPoint);
+        });
+    }
+};
+
+export {
+    clearBlockList,
+    updateDisplay,
+};
